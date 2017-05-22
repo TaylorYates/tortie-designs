f16303329b5a3c3f3fc63b4c8f91bd06b04c3bf5
TaylorYates <taylor.yates@live.com>
2017-05-22 19:16:52 +1000

    Whoops, added all the things to this commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('default', ['sass', 'browserSync'], function(){
+
+});
+
+gulp.task('sass', function(){
+  return gulp.src('assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  });
+
+  gulp.start('watch');
+});
+
+gulp.task('reload', function() {
+  reload();
+});
+
+gulp.task('watch', function(){
+  gulp.watch('assets/scss/*.scss', ['sass']);
+  gulp.watch('*.html', ['reload']);
+  gulp.watch('assets/js/*.js', ['reload']);
+});
